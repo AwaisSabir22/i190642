8630de381a2672b74efd9f299f95c0651f392679
syedhasan65 <i190428@nu.edu.pk>
2022-10-17 12:17:26 -0700

    Thora sa

---
+++ app/src/main/java/com/awaissabir/i190642/additivepage1.java
@@ -0,0 +1,16 @@
+package com.awaissabir.i190642;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class additivepage1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_additivepage1);
+
+
+    }
+}
